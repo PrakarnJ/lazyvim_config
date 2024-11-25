867867651bbda74c7c19de69a1ca936e5cb4dc16
Prakarn Jaroonsorn <prakarnj@Prakarns-MacBook-Pro.local>
2024-11-25 09:36:11 +0700

    Change plugins dashboard to snack

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -154,10 +154,11 @@ return {
   },
 
   {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    opts = function(_, opts)
-      local logo = [[
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
 ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ██████╗ ███╗   ██╗         ██╗   
 ██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██╔══██╗██╔══██╗████╗  ██║         ██║   
 ██████╔╝██████╔╝███████║█████╔╝ ███████║██████╔╝██╔██╗ ██║         ██║   
@@ -165,10 +166,20 @@ return {
 ██║     ██║  ██║██║  ██║██║  ██╗██║  ██║██║  ██║██║ ╚████║    ╚█████╔╝██╗
 ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝     ╚════╝ ╚═╝
                                                                          
-      ]]
-
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-    end,
+]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        },
+      },
+    },
   },
 }
